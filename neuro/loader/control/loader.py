19d92500998eede6b323149f9921911b1d92d135
Rebekka Purcell <rebekkapurcell@rebekkas-air.hdhr>
2021-12-28 17:37:22 -0800

    fix(plot): add plot example

--- neuro/loader/control/loader.py
+++ neuro/loader/control/loader.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from neuro.utils.parse.cmdline import db_cmdline
@@ -16,7 +16,7 @@ def main():
     cmdline = parser.parse_args()
     files = file_utils.find_files(cmdline.data_dir, ".csv", True)
     for f in files:
-        df = pandas.read_csv(f, header=None, skiprows=5)
+        df = pd.read_csv(f, header=None, skiprows=5)
         print(df.loc[0,2])
         print(df)
         ret_val = df
@@ -24,12 +24,23 @@ def main():
 
 def plot():
     #df = main()
+    headers = ['Student', 'RollNumber', 'Grade']
+    students = {'Student': ['Amit', 'Cody', 'Darren', 'Drew'], 
+        'RollNumber': [1, 5, 10, 15],
+        'Grade': ['A', 'C', 'F', 'B']
+    }
+    df = pd.DataFrame(students, columns = headers)
+
+    # displaying the original DataFrame
+    print("Original DataFrame")
+    print(df)
+    
+    # saving as a CSV file
+    df.to_csv('Students.csv', sep ='\t')    
 
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
-    headers = ['Name', 'Age', 'Marks']
-
-    df = pandas.read_csv('student.csv', names=headers)
-    df.set_index('Name').plot()
+    df = pd.read_csv('Students.csv', names=headers)
+    df.set_index('Student').plot()
     plt.show()
